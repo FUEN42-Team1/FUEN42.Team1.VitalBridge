34c555bde5bc642d92de0459295b61c63744cb50
wei_lun tsai <afrad.alan@gmail.com>
2025-07-31 23:16:27 +0800

    1.搬移-Product相關(都還做完) 2.appsetting有校正 資料庫名稱 3.View/ Products 測試顯示。

---
+++ Team1.VitalBridge.BackStage/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Team1.VitalBridge.BackStage.Models.EFModels;
+using Team1.VitalBridge.BackStage.Models.ViewModels;
+
+namespace Team1.VitalBridge.BackStage.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly AppDbContext _context;
+
+		public ProductsController(AppDbContext context)
+		{
+			this._context = context;
+		}
+		public IActionResult Index()
+		{
+			var data = _context.Products
+				.AsNoTracking()
+				.Select(p => p.ToIndexVm())
+				.ToList();
+			return View(data);
+		}
+	}
+}
