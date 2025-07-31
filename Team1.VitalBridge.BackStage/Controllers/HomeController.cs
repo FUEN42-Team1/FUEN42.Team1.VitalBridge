dd5c63ca9c5288bb2fc37530c2a6bdc06835bec7
Alan <afrad.alan@gmail.com>
2025-07-31 16:45:44 +0800

    加入專案檔案。

---
+++ Team1.VitalBridge.BackStage/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Team1.VitalBridge.BackStage.Models;
+
+namespace Team1.VitalBridge.BackStage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult test2_1()
+        {
+            return View();
+        }
+        public IActionResult test2_2()
+        {
+            return View();
+        }
+
+        public IActionResult 沒有子項目()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Team1.VitalBridge.BackStage/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Team1.VitalBridge.BackStage.Models.EFModels;
+
+namespace Team1.VitalBridge.BackStage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // 獲取 appsettings.json 中名為 "DefaultConnection" 的連線字串
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            // 註冊 AppDbContext 服務
+            // 使用 SQL Server 資料庫，並傳入連線字串
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
