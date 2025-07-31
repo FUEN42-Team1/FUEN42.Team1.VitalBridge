a2fe6316c1792bf87b1c82293d06247f9a6f6168
wbjohnny <johnny.westburn@gmail.com>
2025-07-31 19:53:31 +0800

    Create ContentTypeCategoryPairVM

---
+++ Team1.VitalBridge.BackStage/Models/ViewModels/ContentTypeCategoryPairViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Team1.VitalBridge.BackStage.Models.ViewModels
+{
+    public class ContentTypeCategoryPairViewModel
+    {
+    }
+
+    public class ContentTypeCategoryPairDisplayViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Content Type Name")]
+        public string ContentTypeName { get; set; }
+
+        [Display(Name = "CT Order")]
+        public int ContentTypeDisplayOrder { get; set; }
+
+        [Display(Name = "Parent Content Category Name")]
+        public string ParentContentCategoryName { get; set; }
+
+        [Display(Name = "PCC Order")]
+        public int ParentContentCategoryDisplayOrder { get; set; }
+
+        [Display(Name = "Content Category Name")]
+        public string ContentCategoryName { get; set; }
+
+        [Display(Name = "CC Order")]
+        public int ContentCategoryDisplayOrder { get; set; }
+
+        [Display(Name = "IsEnabled")]
+        public string IsEnabledStatus { get; set; }
+    }
+
+    public class ContentTypeCategoryPairCreateViewModel
+    {
+    }
+
+    public class ContentTypeCategoryPairEditViewModel
+    {
+    }
+}
