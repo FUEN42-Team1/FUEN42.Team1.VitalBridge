ad9c7eaed66a261643e5d95b3ba04b80b3a87cda
wbjohnny <johnny.westburn@gmail.com>
2025-07-31 19:53:10 +0800

    Create ContentsController

---
+++ Team1.VitalBridge.BackStage/Controllers/ContentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team1.VitalBridge.BackStage.Controllers
+{
+    public class ContentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContentsIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ContentTypeCategoryPairsIndex()
+        {
+            return View();
+        }
+    }
+}
