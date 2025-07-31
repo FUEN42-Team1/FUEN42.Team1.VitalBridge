32c231a098932564e2ba99a617b4604e0fd42613
wbjohnny <johnny.westburn@gmail.com>
2025-07-31 20:29:06 +0800

    Create directory Servic and DTOs

---
+++ Team1.VitalBridge.BackStage/Models/DTOs/ContentTypeCategoryPairDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Team1.VitalBridge.BackStage.Models.DTOs
+{
+    public class ContentTypeCategoryPairDTO
+    {
+    }
+
+    public class ContentTypeCategoryPairDisplayDTO
+    {
+    }
+
+    public class ContentTypeCategoryPairCreateDTO
+    {
+    }
+
+    public class ContentTypeCategoryPairEditDTO
+    {
+    }
+}
