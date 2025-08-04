e93a8402ab0c93feb0d8b769d2c3d050055910f3
MMMM1925 <mmmm1926@gmail.com>
2025-08-05 00:04:15 +0800

    1.會員新增身分(未完成)

---
+++ Team1.VitalBridge.BackStage/Models/ViewModels/PermissionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Team1.VitalBridge.BackStage.Models.ViewModels
+{
+    public class PermissionViewModel
+    {
+        public string PermissionCode { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+
+    }
+}
