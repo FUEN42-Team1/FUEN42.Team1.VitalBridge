18ddf605ff2cb2fd2427bb920d59fa079ed7cd61
wei_lun tsai <afrad.alan@gmail.com>
2025-08-04 20:38:19 +0800

    -商品分類樹枝狀突出現，ProductCategoryController -刪除之前做的ViewModel

---
+++ Team1.VitalBridge.BackStage/Controllers/ProductCategoryController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Team1.VitalBridge.BackStage.Models.EFModels;
+using Team1.VitalBridge.BackStage.Models.ViewModels;
+
+namespace Team1.VitalBridge.BackStage.Controllers
+{
+	public class ProductCategoryController : Controller
+	{
+		private readonly AppDbContext _context;
+
+		public ProductCategoryController(AppDbContext contexxt)
+		{
+			this._context = contexxt;
+		}
+		// 把要把類別頁面叫出來
+		public IActionResult Index()
+		{
+			// 1. 取得所有類別資料
+
+			var categories = _context.Categories
+				.ToList();
+
+			// 2. 轉換成ViewModel並計算層級
+
+			var categoryTreelist = new List<ProductCategoryTreeViewModel>();
+
+			// 3. 先處理第一層（ParentId == null）
+			var rootCategories = categories
+				.Where(c => c.FatherId == null).ToList();
+			foreach(var root in rootCategories)
+			{
+				categoryTreelist.Add(new ProductCategoryTreeViewModel
+				{
+					Id = root.Id,
+					name= root.Name,
+					FatherId = root.FatherId,
+					IsActive = root.IsActive,
+					Level=0
+
+				});
+
+				// 4. 處理子類別
+				// 在categories 裡面 篩選出 FatherId 已經有填的，也就是上面的root.Id
+				// 因為root.id 代表已經是父類別，這邊就是在找所有root的子類別
+				var children = categories.Where (c =>c.FatherId ==root.Id).ToList();
+				foreach (var child in children)
+				{
+					categoryTreelist.Add(new ProductCategoryTreeViewModel
+					{
+						Id = child.Id,
+						name = child.Name,
+						FatherId = child.FatherId,
+						IsActive = child.IsActive,
+						Level = 1
+
+					});
+
+					//5. 第三層
+					var childrenthird = categories.Where(c => c.FatherId == child.Id).ToList();
+					foreach (var childthree in childrenthird)
+					{
+						categoryTreelist.Add(new ProductCategoryTreeViewModel
+						{
+							Id = childthree.Id,
+							name = childthree.Name,
+							FatherId = childthree.FatherId,
+							IsActive = childthree.IsActive,
+							Level = 2
+
+						});
+
+					}
+
+				}
+
+
+
+			}
+
+
+
+
+
+			return View(categoryTreelist);
+		}
+
+
+
+
+	}
+}
---
+++ Team1.VitalBridge.BackStage/Models/ViewModels/ProductCateforyFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team1.VitalBridge.BackStage.Models.ViewModels
+{
+	public class ProductCateforyFormViewModel
+	{
+		// 這是用來新增、修改 類別
+		
+		public int? FatherId { get; set; }
+
+		[Required]
+		public string name { get; set; }
+		public bool IsActive { get; set; } = true; // 預設開啟
+		public int Level { get; set; } // 用於縮排顯示
+
+		public List<SelectListItem>? ParentOptions { get; set; }  // 用於下拉式選單
+
+
+	}
+}
