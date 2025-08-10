b01395cd3cf236922d64086c5a0686fce4221905
MMMM1925 <mmmm1926@gmail.com>
2025-08-10 17:11:14 +0800

    1.機構法人清單(清單/審核) 2.機構註冊流程(還沒做圖片上傳的部分) 3.機構審核(還沒做圖片讀取的部分)

--- Team1.VitalBridge.BackStage/Controllers/Institutions/InstitutionHomeController.cs
+++ Team1.VitalBridge.BackStage/Controllers/Institutions/InstitutionHomeController.cs
@@ -7,9 +7,25 @@ namespace Team1.VitalBridge.BackStage.Controllers.Institutions
     [Route("Institution/[controller]/[action]")]
     public class InstitutionHomeController : Controller
     {
+        //機構後臺主頁
         public IActionResult Index()
         {
             return View();
         }
+
+        //審核駁回重新填寫流程
+        public IActionResult ReApply()
+        {
+            return View();
+        }
+
+        //機構基本資料
+
+
+        //機構員工管理(自己的)
+
+
+
+
     }
 }
