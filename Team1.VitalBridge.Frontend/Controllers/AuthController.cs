da59f36e4624d841fbde15b0db0df03903d677bf
MMMM1925 <mmmm1926@gmail.com>
2025-08-16 23:14:30 +0800

    1.安裝套件(BCrypt、Jwt、EFCoreTool)

---
+++ Team1.VitalBridge.Frontend/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Team1.VitalBridge.Frontend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+
+
+
+
+    }
+}
