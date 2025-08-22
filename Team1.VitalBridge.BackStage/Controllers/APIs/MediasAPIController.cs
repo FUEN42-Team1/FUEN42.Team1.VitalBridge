82701a9fbdcedfcf9f898b98cfe596ba36347633
Johnny <johnny.westburn@gmail.com>
2025-08-22 16:31:05 +0800

    save ckeditor upload api fix

--- Team1.VitalBridge.BackStage/Controllers/APIs/MediasAPIController.cs
+++ Team1.VitalBridge.BackStage/Controllers/APIs/MediasAPIController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using MvcTips.Site.Models.Utilities;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.InteropServices;
 using Team1.VitalBridge.BackStage.Models.EFModels;
 using FileStream = System.IO.FileStream;
 
@@ -15,6 +18,10 @@ namespace Team1.VitalBridge.BackStage.Controllers.APIs
     {
         private readonly AppDbContext _context;
         private readonly IWebHostEnvironment _env;
+        private const string FileTableUNCPath = @"\\40.76.107.125\mssqlserver\VitalBridgeDB\MyFileTableDir";
+        private const string FileTableUser = "prjTeam1";
+        private const string FileTablePassword = "VitalBridge123";
+        private const string FileTableDomain = "prjTeam1";
 
         public MediasAPIController(IWebHostEnvironment env, AppDbContext context)
         {
@@ -22,84 +29,44 @@ namespace Team1.VitalBridge.BackStage.Controllers.APIs
             this._context = context;
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> UploadMedia(IFormFile upload)
-        //{
-        //    if (upload == null || upload.Length == 0)
-        //        return BadRequest("No file uploaded.");
-
-        //    // 1. Save to FileTable folder path
-        //    var fileTablePath = @"\\localhost\mssqlserver\VitalBridgeDB\MyFileTableDir";
-
-        //    // 取原始副檔名
-        //    string extension = Path.GetExtension(upload.FileName);
-        //    // 使用 Guid 產生新檔名
-        //    string fileName = $"{Guid.NewGuid().ToString("N")}{extension}";
-
-        //    string filePath = Path.Combine(fileTablePath, fileName);
-        //    if (!Directory.Exists(fileTablePath))
-        //    {
-        //        Directory.CreateDirectory(fileTablePath);
-        //    }
-
-        //    using (var stream = new FileStream(filePath, FileMode.Create))
-        //    {
-        //        await upload.CopyToAsync(stream);
-        //    }
-
-        //    // 2. Create Media record
-        //    var media = new Media
-        //    {
-        //        Name = fileName,
-        //        FileId = _context.FileStreams.FirstOrDefault(f => f.FileName == fileName).Id,
-        //        CreatedAt = DateTime.Now
-        //    };
-        //    _context.Medias.Add(media);
-        //    await _context.SaveChangesAsync();
-
-        //    //// 3. Return the URL for CKEditor to display
-        //    //return Ok(new
-        //    //{
-        //    //    uploaded = true,
-        //    //    url = $"/media/{media.FileId}" // Your download endpoint
-        //    //});
-        //    return Ok();
-        //}
-
+        
         [HttpPost]
         public async Task<IActionResult> UploadFile([FromForm] IFormFile upload)
         {
             try
             {
-                var fileTablePath = @"\\localhost\mssqlserver\VitalBridgeDB\MyFileTableDir";
-
-                if (upload == null || upload.Length == 0)
+                using (new NetworkConnection(FileTableUNCPath, new NetworkCredential(FileTableUser, FileTablePassword, FileTableDomain)))
                 {
-                    return null;
+                    var fileTablePath = FileTableUNCPath;
+
+                    if (upload == null || upload.Length == 0)
+                    {
+                        return null;
+                    }
+
+                    // 取原始副檔名
+                    string extension = Path.GetExtension(upload.FileName);
+                    // 使用 Guid 產生新檔名
+                    string fileName = $"{Guid.NewGuid().ToString("N")}{extension}";
+
+                    string filePath = Path.Combine(fileTablePath, fileName);
+                    if (!Directory.Exists(fileTablePath))
+                    {
+                        Directory.CreateDirectory(fileTablePath);
+                    }
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await upload.CopyToAsync(stream);
+                    }
+                    var FileId = _context.FileStreams.FirstOrDefault(f => f.FileName == fileName)?.Id;
+
+                    return Ok(new
+                    {
+                        uploaded = true,
+                        url = $"/api/MediasAPI/{FileId}"
+                    });
                 }
-
-                // 取原始副檔名
-                string extension = Path.GetExtension(upload.FileName);
-                // 使用 Guid 產生新檔名
-                string fileName = $"{Guid.NewGuid().ToString("N")}{extension}";
-
-                string filePath = Path.Combine(fileTablePath, fileName);
-                if (!Directory.Exists(fileTablePath))
-                {
-                    Directory.CreateDirectory(fileTablePath);
-                }
-
-                using (var stream = new FileStream(filePath, FileMode.Create))
-                {
-                    await upload.CopyToAsync(stream);
-                }
-                var FileId = _context.FileStreams.FirstOrDefault(f => f.FileName == fileName)?.Id;
-
-                return Ok(new
-                {
-                    uploaded = true,
-                    url = $"/api/MediasAPI/{FileId}"
-                });
             }
             catch (Exception ex)
             {
@@ -117,22 +84,25 @@ namespace Team1.VitalBridge.BackStage.Controllers.APIs
         [HttpGet("{fileId}")]
         public IActionResult GetFile(int fileId)
         {
-            var fileName = _context.FileStreams
+            using (new NetworkConnection(FileTableUNCPath, new NetworkCredential(FileTableUser, FileTablePassword, FileTableDomain)))
+            {
+                var fileName = _context.FileStreams
                 .Where(f => f.Id == fileId)
                 .Select(f => f.FileName)
                 .FirstOrDefault();
-            var safeFileName = Path.GetFileName(fileName); // 防止路徑穿越
-            var fileTablePath = @"\\localhost\mssqlserver\VitalBridgeDB\MyFileTableDir";
-            var filePath = Path.Combine(fileTablePath, safeFileName);
 
-            if (!System.IO.File.Exists(filePath))
-                return NotFound();
-            
+                var safeFileName = Path.GetFileName(fileName); // 防止路徑穿越
+                var fileTablePath = FileTableUNCPath;
+                var filePath = Path.Combine(fileTablePath, safeFileName);
 
-            var contentType = GetContentType(filePath); // ✅ 正確 MIME 類型
-            var fileBytes = System.IO.File.ReadAllBytes(filePath);
-            return File(fileBytes, contentType, fileName);
-            //return File(fileBytes, contentType);
+                if (!System.IO.File.Exists(filePath))
+                    return NotFound();
+
+
+                var contentType = GetContentType(filePath); // ✅ 正確 MIME 類型
+                var fileBytes = System.IO.File.ReadAllBytes(filePath);
+                return File(fileBytes, contentType, fileName);
+            }
         }
         private string GetContentType(string path)
         {
@@ -154,6 +124,53 @@ namespace Team1.VitalBridge.BackStage.Controllers.APIs
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+        public class NetworkConnection : IDisposable
+        {
+            private readonly string _networkName;
+
+            public NetworkConnection(string networkName, NetworkCredential credentials)
+            {
+                _networkName = networkName;
+
+                var netResource = new NetResource
+                {
+                    Scope = 2, // Global
+                    Type = 1,  // Disk
+                    DisplayType = 3,
+                    RemoteName = networkName
+                };
+
+                var result = WNetAddConnection2(netResource, credentials.Password,
+                    $@"{credentials.Domain}\{credentials.UserName}", 0);
+
+                if (result != 0)
+                    throw new Win32Exception(result);
+            }
+
+            public void Dispose()
+            {
+                WNetCancelConnection2(_networkName, 0, true);
+            }
+
+            [DllImport("mpr.dll")]
+            private static extern int WNetAddConnection2(NetResource netResource, string password, string username, int flags);
+
+            [DllImport("mpr.dll")]
+            private static extern int WNetCancelConnection2(string name, int flags, bool force);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public class NetResource
+            {
+                public int Scope;
+                public int Type;
+                public int DisplayType;
+                public int Usage;
+                public string LocalName;
+                public string RemoteName;
+                public string Comment;
+                public string Provider;
+            }
         }
     }
 }
